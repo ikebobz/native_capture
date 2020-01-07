3608741b15d77d893528242c7a3a40f72f11658b
ikebobs <ikebobs>
2020-01-07 22:16:15 +0100

    i love working with git

---
+++ src/fxmlexample/JScripts.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capturebyfx;
+
+/**
+ *
+ * @author ak
+ */
+public class JScripts 
+{
+  public JScripts()
+  {
+  
+  
+  }
+  public static String code1 = "$('#NonDeliveryMeasure').removeAttr('disabled');var values = ['B','F'];var options = ['B - Scheduled for further action/processing on next working day','F - Item forwarded/redirected'];for(var i=0;i<2;i++){$('<option/>',{value:values[i],html:options[i]}).appendTo('#NonDeliveryMeasure')}";
+}
